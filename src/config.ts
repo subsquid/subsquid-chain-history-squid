b31f49d6ff198978ecbbd0290dffc5a7c377d10f
Alexander Belopashentsev <abelopashentsev@gmail.com>
2022-06-30 19:08:09 +0500

    chore: change wss endpoint

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ const config: ProcessorConfig = {
     prefix: 'moonbeam',
     dataSource: {
         archive: 'https://moonbeam.archive.subsquid.io/graphql',
-        chain: 'wss://moonbeam.api.onfinality.io/public-ws',
+        chain: 'wss://rpc.pinknode.io/moonbeam/6e3fa591-e24f-483a-95fa-1d773f7f2be3',
     },
     typesBundle: 'moonbeam',
     batchSize: 500,
