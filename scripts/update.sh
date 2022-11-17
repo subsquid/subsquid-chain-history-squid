978b51b0ea8b45b1516022122485047703b71232
Alexander Belopashentsev <61732514+belopash@users.noreply.github.com>
2022-11-17 17:58:16 +0500

    chore: comment unimplemented chains in update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,9 +1,8 @@
 sqd squid:update kusama-balances@v1 -e CHAIN=kusama
-sqd squid:update polkadot-balances@v1 -e CHAIN=moonriver
 sqd squid:update karura-balances@v1 -e CHAIN=karura
-sqd squid:update bifrost-balances@v1 -e CHAIN=bifrost
+# sqd squid:update bifrost-balances@v1 -e CHAIN=bifrost
 
 sqd squid:update polkadot-balances@v1 -e CHAIN=polkadot
-sqd squid:update polkadot-balances@v1 -e CHAIN=moonbeam
 sqd squid:update acala-balances@v1 -e CHAIN=acala
-sqd squid:update phala-balances@v1 -e CHAIN=phala
+# sqd squid:update phala-balances@v1 -e CHAIN=phala
+# sqd squid:update crust-balances@v1 -e CHAIN=crust
